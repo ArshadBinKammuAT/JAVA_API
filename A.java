896c5e46befc9a21fd2260d95e4e2df1715bde9c
ARSHAD A T <115610085+ArshadBinKammuAT@users.noreply.github.com>
2023-11-17 00:37:43 +0530

    Create A.java

---
+++ A.java
@@ -0,0 +1,19 @@
+import com.silicon.A;
+
+public static void test(){
+	System.out.println(A.a);
+	int i=A.go();
+	System.out.println(i);
+	
+	A r=new A(true);
+	System.out.println(r.b);
+	char j=r.went(23.4F);
+	System.out.println(j);
+}
+pubic static void main(String[] args){
+	test();
+}
+
+	
+	
+	
