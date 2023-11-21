461c9daf6ce11c2ad5370a418c4a8c09e58cd77f
ARSHAD A T <115610085+ArshadBinKammuAT@users.noreply.github.com>
2023-11-21 19:37:33 +0530

    Update EH1.java

--- EH1.java
+++ EH1.java
@@ -31,3 +31,29 @@ Test:::::  java.lang.ArithmeticException: / by zero
 DEMO:::: java.lang.StringIndexOutOfBoundsException: Index 8 out of bounds for length 1
 inner finally block
 finally block
+
+
+// only affects the exception line
+class Eh{
+	public static void main(String args[]){
+		try{
+			int a=100/10;
+			System.out.println(a);
+				String s1="A";
+				char c =s1.charAt(8);
+				System.out.println(c);
+		}
+		catch(Exception e){
+			System.out.println("Test:::::  "+e);		
+		}finally{
+			System.out.println("finally block");
+		}
+		}
+	}
+
+
+
+//Output
+10
+Test:::::  java.lang.StringIndexOutOfBoundsException: Index 8 out of bounds for length 1
+finally block
