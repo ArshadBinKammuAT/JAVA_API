b8a360c17d43f13b6f74a32780b1cdb9c006abec
ARSHAD A T <115610085+ArshadBinKammuAT@users.noreply.github.com>
2023-11-22 15:31:45 +0530

    Create StringBuffer & StringBuilder.java

---
+++ StringBuffer & StringBuilder.java
@@ -0,0 +1,25 @@
+/
+
+
+
+
+
+
+
+
+
+//to delete some character from string
+
+class Eh{
+	public static void main(String args[]){
+		StringBuffer sb=new StringBuffer("Arshad Achuthodika");
+		System.out.println(sb);
+		sb.delete(3,10);
+		System.out.println(sb);
+	}
+}
+		
+//output
+abc
+
+
